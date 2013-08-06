ec1ff51af26774eb5c8173395bbbf7a42c7c022e
Ben Holloway <ben.mai>
2013-08-06 14:42:38 +0100

    major commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+console.log(mpdata.length);
+
+var policies = _.without(_.unique(
+	_.flatten(
+		_.map(mpdata, function(mp){ return _.keys(mp); })
+	)
+), 'name');
+
+console.log(policies);
+
+var items = [];
+$.each(policies, function(i, pol){
+	var tr = $('<tr></tr>').addClass('policy')
+		.data('policy', pol)
+		.prepend('<td class="poli"><p class="lead">'+pol+'</p></td>')
+		.prepend("<td><div style='margin-top:16px' class='btn-group' data-toggle='buttons'><td class='opti'><label class='btn btn-success'><input type='radio' name='options' id='option1'>For </input></label></td><td class='opti'><label class='btn btn-default'><input type='radio' name='options' id='option2'>Unsure </input></label></td><td class='opti'><label class='btn btn-danger'><input type='radio' name='options' id='option1'>Against </input></label></td></div></td>");
+	
+	items.push(tr);
+
+});
+$('.policies').append(items);
--- names-of-options.m
+++ names-of-options.m
@@ -1,1 +1,3 @@
 euthanasia 1
+foxhunting 2
+communityschools 3
