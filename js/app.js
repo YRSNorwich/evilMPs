c353f9a4a866423cf133aa38e705b0f7ebf2a8ef
Ben Holloway <ben.mai>
2013-08-06 15:26:05 +0100

    adding-submit

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ $.each(policies, function(i, pol){
 	var tr = $('<tr></tr>').addClass('policy')
 		.data('policy', pol)
 		.prepend('<td class="poli"><p class="lead">'+pol+'</p></td>')
-		.prepend("<td><div style='margin-top:16px' class='btn-group' data-toggle='buttons'><td class='opti'><label class='btn btn-success'><input type='radio' name='options' id='option1'>For </input></label></td><td class='opti'><label class='btn btn-default'><input type='radio' name='options' id='option2'>Unsure </input></label></td><td class='opti'><label class='btn btn-danger'><input type='radio' name='options' id='option1'>Against </input></label></td></div></td>");
+		.prepend("<td><div style='margin-top:16px' class='btn-group' data-toggle='buttons'><td class='opti'><label class='btn btn-success'><input type='radio' name='options' id='option1'> For </input></label></td><td class='opti'><label class='btn btn-default'><input type='radio' name='options' id='option2'> Unsure </input></label></td><td class='opti'><label class='btn btn-danger'><input type='radio' name='options' id='option1'> Against </input></label></td></div></td>");
 	
 	items.push(tr);
 
---
+++ sql.php
@@ -0,0 +1,19 @@
+<?php
+
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); 
+header("Cache-Control: no-store, no-cache, must-revalidate"); 
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+$foxhunting = $_POST["1"];
+$abortion = $_POST["2"];
+$eu = $_POST["3"];
+
+$pad = "&nbsp;&nbsp;&nbsp;";
+
+echo $foxhunting;
+echo $abortion;
+echo $eu;
+
+?>
